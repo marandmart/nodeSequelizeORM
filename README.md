8e057da4688b465e8f0832015a4dfe873f5a1195
Mario Andre Martins <mario.andre.m@gmail.com>
2023-08-30 19:51:18 -0300

    created a table, updated readme and changed directories on sequelizerc file

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
-porta utilizada pelo mysql: 8000
+# Project details
 
-Criar o arquivo .env e configurar uma PORT para o projeto
+PORT used to access MySQL: 8000
 
-subir o container do mysql:
+# Setup
+
+- Create a .env file to setup desired port for the node project
+
+- Create the MySQL using the Dockerfile with the following command inside the mysql folder
 
 ```
 docker build -t mysql-server .
 ```
 
 ```
-docker run -dp 8000:3306 -v $(pwd)/database:/database --name mysql mysql-server
+docker run -d -p 8000:3306 --name mysql -v database:/var/lib/mysql mysql-server
 ```
---
+++ src/migrations/20230830222932-create-people.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('People', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      p_name: {
+        type: Sequelize.STRING
+      },
+      is_active: {
+        type: Sequelize.BOOLEAN
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      p_role: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('People');
+  }
+};
