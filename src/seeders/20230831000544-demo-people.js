4393d30e0d99db3c68f271e1b1079882516a8686
Mario Andre Martins <mario.andre.m@gmail.com>
2023-08-30 21:25:26 -0300

    created a seeder with mock data for people

--- src/seeders/20230831000544-demo-people.js
+++ src/seeders/20230831000544-demo-people.js
@@ -1,25 +1,66 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  // insert a group of people onto the database
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "People",
+      [
+        {
+          p_name: "Ana Souza",
+          is_active: true,
+          email: "ana@ana.com",
+          p_role: "estudante",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          p_name: "Marcos Cintra",
+          is_active: true,
+          email: "marcos@marcos.com",
+          p_role: "estudante",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          p_name: "Felipe Cardoso",
+          is_active: true,
+          email: "felipe@felipe.com",
+          p_role: "estudante",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          p_name: "Sandra Silva",
+          is_active: false,
+          email: "sandra@sandra.com",
+          p_role: "estudante",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          p_name: "Paula Moraes",
+          is_active: true,
+          email: "paula@paula.com",
+          p_role: "docente",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          p_name: "Sergio Lopes",
+          is_active: true,
+          email: "sergio@sergio.com",
+          p_role: "docente",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("People", null, {});
+  },
 };
