145492242e9f24143e6e20040303894332a138d1
Mário André Martins <mario.andre.m@gmail.com>
2023-09-10 07:30:28 -0300

    added enrollment routes and methods

--- src/controllers/PeopleController.ts
+++ src/controllers/PeopleController.ts
@@ -2,7 +2,7 @@ const database = require("../models");
 import express from "express";
 
 class PeopleController {
-  static async getAll(req: express.Request, res: express.Response) {
+  static async getAllPeople(req: express.Request, res: express.Response) {
     try {
       const allPeople = await database.People.findAll();
       return res.status(200).json(allPeople);
@@ -74,6 +74,79 @@ class PeopleController {
       return res
         .status(200)
         .json({ message: `Person with id ${id} successfully deleted` });
+    } catch (error: any) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async getEnrollment(req: express.Request, res: express.Response) {
+    const { s_id, enrollment_id } = req.params;
+    try {
+      const enrollment = await database.Enrollment.findOne({
+        where: { id: Number(enrollment_id), s_id: Number(s_id) },
+      });
+      if (enrollment) return res.status(200).json(enrollment);
+      else return res.status(204);
+    } catch (error: any) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async enroll(req: express.Request, res: express.Response) {
+    const { s_id } = req.params;
+    const { curr_status = "enrolled", class_id } = req.body;
+    try {
+      if (s_id && class_id) {
+        const newEnrollment = await database.Enrollment.create({
+          curr_status,
+          s_id: Number(s_id),
+          class_id,
+        });
+        return res.status(200).json(newEnrollment);
+      } else {
+        return res.status(204);
+      }
+    } catch (error: any) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async updateEnrollment(req: express.Request, res: express.Response) {
+    const { s_id, enrollment_id } = req.params;
+    const enrollmentUpdate = req.body;
+    const { curr_status, class_id } = enrollmentUpdate;
+    try {
+      if (curr_status || class_id) {
+        await database.Enrollment.update(enrollmentUpdate, {
+          where: {
+            id: Number(enrollment_id),
+            s_id: Number(s_id),
+          },
+        });
+        const updatedEnrollment = await database.Enrollment.findOne({
+          where: { id: Number(enrollment_id), s_id: Number(s_id) },
+        });
+        return res.status(200).json(updatedEnrollment);
+      } else {
+        return res.status(204);
+      }
+    } catch (error: any) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async unenroll(req: express.Request, res: express.Response) {
+    const { s_id, enrollment_id } = req.params;
+    try {
+      await database.Enrollment.destroy({
+        where: {
+          id: Number(enrollment_id),
+          s_id: Number(s_id),
+        },
+      });
+      return res.status(200).json({
+        message: `Enrollment with id ${enrollment_id} successfully deleted`,
+      });
     } catch (error: any) {
       return res.status(500).json(error.message);
     }
--- src/routes/peopleRoutes.ts
+++ src/routes/peopleRoutes.ts
@@ -4,9 +4,22 @@ import PeopleController from "../controllers/PeopleController.js";
 const router = Router();
 
 router.get("/people/:id", PeopleController.getPerson);
-router.get("/people", PeopleController.getAll);
+router.get("/people", PeopleController.getAllPeople);
 router.post("/people", PeopleController.addPerson);
 router.put("/people/:id", PeopleController.updatePerson);
 router.delete("/people/:id", PeopleController.removePerson);
+router.get(
+  "/people/:s_id/enrollment/:enrollment_id",
+  PeopleController.getEnrollment
+);
+router.post("/people/:s_id/enrollment", PeopleController.enroll);
+router.put(
+  "/people/:s_id/enrollment/:enrollment_id",
+  PeopleController.updateEnrollment
+);
+router.delete(
+  "/people/:s_id/enrollment/:enrollment_id",
+  PeopleController.unenroll
+);
 
 export default router;
